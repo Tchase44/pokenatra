670ebf1679de7734450dc724e7b87e95aac56f0a
Tim Chase <tchase4544@gmail.com>
2017-05-31 10:47:29 -0400

    pokemon mvp

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,11 @@
 DROP TABLE IF EXISTS pokemons;
+DROP TABLE IF EXISTS trainers;
 
 CREATE TABLE pokemons(
 	id SERIAL PRIMARY KEY,
 	name VARCHAR(50),
 	cp INT,
 	poke_type VARCHAR(50),
-	img_url TEXT
+	img_url TEXT,
 );
-
-
 
